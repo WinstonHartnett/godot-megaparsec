3481f68337e6b02192457963e29051a014aa43b8
Winston Hartnett <winstonhartnett@gmail.com>
2021-10-29 02:08:14 -0500

    Renaming

--- bench/Main.hs
+++ bench/Main.hs
@@ -2,11 +2,9 @@
 
 module Main where
 
-import           Control.Monad         (unless)
-
 import           Criterion.Main
 
-import           Data.Either           (fromRight, isRight)
+import           Data.Either           (fromRight)
 import qualified Data.Text.IO          as T
 
 import           Godot.Parser.Resource
@@ -15,7 +13,7 @@ import qualified Text.Megaparsec       as P
 
 main :: IO ()
 main = do
-  let parseFile f = fromRight undefined . P.parse tscnParser "" <$> T.readFile f
+  let parseFile f = fromRight (error $ "Unable to parse " <> f) . P.parse tscnParser "" <$> T.readFile f
   defaultMain [ bench "EntityMap" $ whnfIO (parseFile "bench/EntityMap.tscn")
               , bench "Geography" $ whnfIO (parseFile "bench/Geography.tscn")
               , bench "BigFile" $ whnfIO (parseFile "bench/BigFile.tscn")]
