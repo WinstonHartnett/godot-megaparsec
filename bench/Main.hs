d67f494dc188f19018f249f8f5b6e897542e5aea
Winston Hartnett <winstonhartnett@gmail.com>
2021-11-03 17:25:59 -0500

    Added other parser, more docs

--- bench/Main.hs
+++ bench/Main.hs
@@ -13,7 +13,11 @@ import qualified Text.Megaparsec       as P
 
 main :: IO ()
 main = do
-  let parseFile f = fromRight (error $ "Unable to parse " <> f) . P.parse tscnParser "" <$> T.readFile f
+  let parseFile f =
+        fromRight (error $ "Unable to parse " <> f) . P.parse godotParser ""
+        <$> T.readFile f
   defaultMain [ bench "EntityMap" $ whnfIO (parseFile "bench/EntityMap.tscn")
               , bench "Geography" $ whnfIO (parseFile "bench/Geography.tscn")
+              , bench "MarketActor" $ whnfIO (parseFile "bench/MarketActor.tscn")
+              , bench "Other" $ whnfIO (parseFile "bench/Other.other")
               , bench "BigFile" $ whnfIO (parseFile "bench/BigFile.tscn")]
