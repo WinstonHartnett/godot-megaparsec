8309712f7b5048f4482785572eabb2a60725c81e
Winston Hartnett <winstonhartnett@gmail.com>
2022-06-18 01:57:23 -0500

    Complete refactor

--- bench/Main.hs
+++ bench/Main.hs
@@ -2,22 +2,22 @@
 
 module Main where
 
-import           Criterion.Main
-
-import           Data.Either           (fromRight)
-import qualified Data.Text.IO          as T
-
-import           Godot.Parser.Resource
-
-import qualified Text.Megaparsec       as P
+import Criterion.Main
+import qualified Data.Text.IO as T
+import Godot.Parser.Resource
+import qualified Text.Megaparsec as P
 
 main :: IO ()
 main = do
-  let parseFile f =
-        fromRight (error $ "Unable to parse " <> f) . P.parse godotParser ""
-        <$> T.readFile f
-  defaultMain [ bench "EntityMap" $ whnfIO (parseFile "bench/EntityMap.tscn")
-              , bench "Geography" $ whnfIO (parseFile "bench/Geography.tscn")
-              , bench "MarketActor" $ whnfIO (parseFile "bench/MarketActor.tscn")
-              , bench "Other" $ whnfIO (parseFile "bench/Other.other")
-              , bench "BigFile" $ whnfIO (parseFile "bench/BigFile.tscn")]
+  let parseFile f = do
+        txt <- T.readFile f
+        case P.parse parsedP "" txt of
+          Right p -> pure p
+          Left e -> putStrLn (P.errorBundlePretty e) >> undefined
+  defaultMain
+    [ bench "EntityMap" $ whnfIO (parseFile "bench/EntityMap.tscn")
+    , bench "Geography" $ whnfIO (parseFile "bench/Geography.tscn")
+    , bench "MarketActor" $ whnfIO (parseFile "bench/MarketActor.tscn")
+    , bench "Other" $ whnfIO (parseFile "bench/Other.other")
+    , bench "BigFile" $ whnfIO (parseFile "bench/BigFile.tscn")
+    ]
